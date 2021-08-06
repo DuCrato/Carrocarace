597786d9af79aba71782dfd193dd2cdda3c2d5ae
DuCrato <williamanderson1994@hotmai.com>
2021-08-06 00:25:19 -0300

    Primeiro logica aplicada

--- Carroçao Race codificação.c
+++ Carroçao Race codificação.c
@@ -1,9 +1,7 @@
-#include<stdio.H>
+#include<stdio.h>
 #include<time.h>
-#include<unistd.h>
 #include<stdlib.h>
 #include<windows.h>
-#include<conio.h>
 
 #define COLUNA 11
 #define LINHA 15
@@ -28,27 +26,27 @@ void tela(){
         for(j = 0; j < HORIZOTE; j++){
 
             if(i == 0){
-                
+
                 gotoxy(j, i);
                 printf("%c",220);
 
-            }else if(j == 0){ 
-                
+            }else if(j == 0){
+
                 gotoxy(j, i);
                 printf("%c",219);
 
             }else if(j == HORIZOTE - 1){
-                
+
                 gotoxy(j, i);
                 printf("%c",219);
 
             }else if(i == VERTICAL - 1){
-                
+
                 gotoxy(j, i);
                 printf("%c",220);
 
             }else{
-                
+
                 gotoxy(i, j);
                 printf(" ");
                 }
@@ -83,26 +81,90 @@ int menu(){ // MENU Inicial
     return opcao;
 }
 
-void jogo_pista(){
+static char pista[LINHA][COLUNA];
+static int mover = 0, lado, cimaBaixo = 0, contador = 0;
 
-    int pista[15][11];
+void movimentacao()
+{
+	mover = getch();
+	if(mover == 119){ //W
+
+        if(cimaBaixo != LINHA - 4){
+            ++cimaBaixo;
+            void carregaPista();
+        }
+	}
+	else if(mover == 97){ //A
+
+        if(lado != -4){
+            --lado;
+            void carregaPista();
+        }
+	}
+	else if(mover == 115){ // S
+
+        if(cimaBaixo != 0){
+            --cimaBaixo;
+            void carregaPista();
+        }
+	}
+	else if(mover == 100){ //D
+
+			if(lado != COLUNA - 7){
+            ++lado;
+            void carregaPista();
+        }
+    }
+}
+
+void carregaPista(){
+
     int i, j;
 
-    tela();
+    for(i = 0; i < LINHA; i++){
+        for(j = 0; j < COLUNA; j++){pista[i][j] = ' ';}
+    }
+}
+
+void carroUsuario(){
+
+    pista[LINHA - 1 - cimaBaixo][4 + lado] = 219;
+    pista[LINHA - 1 - cimaBaixo][6 + lado] = 219;
+    pista[LINHA - 2 - cimaBaixo][5 + lado] = 219;
+    pista[LINHA - 3 - cimaBaixo][4 + lado] = 219;
+    pista[LINHA - 3 - cimaBaixo][6 + lado] = 219;
+    pista[LINHA - 4 - cimaBaixo][5 + lado] = 219;
+}
+
+void oponente1(){
+
+    pista[-4 + contador][1] = 219;
+    pista[-1 + contador][0] = 219;
+    pista[-1 + contador][2] = 219;
+    pista[-2 + contador][1] = 219;
+    pista[-3 + contador][0] = 219;
+    pista[-3 + contador][2] = 219;
+}
+void oponente2(){
+
+    pista[-4 + contador][5] = 219;
+    pista[-1 + contador][4] = 219;
+    pista[-1 + contador][6] = 219;
+    pista[-2 + contador][5] = 219;
+    pista[-3 + contador][4] = 219;
+    pista[-3 + contador][6] = 219;
+}
+
+void imprime(){
+
+    int i, j;
 
     for(i = 0; i < LINHA; i++){
-
-        gotoxy(35, 8 + i);
-
         for(j = 0; j < COLUNA; j++){
-           
-            if(j == 0 || j == COLUNA - 1){
-                
-                textcolor(1);
-                printf("%c",219);
-            }else{printf(" ");}
+
+            printf("%c",pista[i][j]);
         }
-         puts("");
+        puts(" ");
     }
 }
 
@@ -111,7 +173,7 @@ void placar(){ // PLACAR a ideia é usar arquivo STTS Zero
     printf("\n\n\tPLACAR EM CONSTRUCAO\n\n");
 }
 
-void instrucao(){ // Como jogar!!!
+void instrucao(){ // Como jogar!!! *** OK ***
 
     int opcao;
 
@@ -122,9 +184,9 @@ void instrucao(){ // Como jogar!!!
     printf("\n\n\t\tTeclas para movimentacao W, A, S e M.\n"
             "\t\tTambem pode ser usadas as setas do teclado.\n"
             "\t\tO objetivo do JOGO eh passar a maior quantidade de tempo.\n\n");
-  
+
     gotoxy(0,20);
-  
+
     printf("\t\t\tJogar [1] ou Menu pricipal [0]\n");
     printf("\t\t\tDigite a Opcao: ");
         scanf("%d",&opcao);
@@ -139,7 +201,7 @@ void load(){
     for(i = 0; i < 10; i++){
 
         sleep(1);
-        printf("%c",219);       
+        printf("%c",219);
     }
 }
 void sair(){
@@ -150,7 +212,7 @@ void sair(){
     printf("SAINDO ");
     load();
 }
-void carregando(){
+void carregando(){ //
 
     tela();
 
@@ -162,7 +224,20 @@ void carregando(){
 
 int main(){
 
-    jogo_pista();
+    int i;
 
+    for(i = 1; i != 0; i++){
+
+        system("cls");
+        carregaPista();
+        oponente1();
+        oponente2();
+        carroUsuario();
+        imprime();
+        movimentacao();
+        if(contador != 16){contador++;
+        }else{contador = 0;}
+
+    }
     return 0;
 }
